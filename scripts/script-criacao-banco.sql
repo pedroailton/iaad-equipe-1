24bb8027cba2561f345d3069772ed974f067756f
Pedro Ailton <pedroailton630@gmail.com>
2026-05-29 13:28:24 -0300

    feat: diagrama DER + script de criação do banco

---
+++ scripts/script-criacao-banco.sql
@@ -0,0 +1,105 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`selecoes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`selecoes` (
+  `id_selecao` INT NOT NULL,
+  `nome_selecao` VARCHAR(50) NOT NULL,
+  `continente` VARCHAR(50) NOT NULL,
+  `tecnico` VARCHAR(50) NOT NULL,
+  `titulos` INT NOT NULL,
+  PRIMARY KEY (`id_selecao`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`jogadores`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`jogadores` (
+  `id_jogador` INT NOT NULL,
+  `nome_jogador` VARCHAR(60) NOT NULL,
+  `posicao` VARCHAR(30) NOT NULL,
+  `numero_camisa` INT NOT NULL,
+  `data_nascimento` DATE NOT NULL,
+  `id_selecao` INT NOT NULL,
+  PRIMARY KEY (`id_jogador`),
+  INDEX `fk_jogadores_selecoes_idx` (`id_selecao` ASC) INVISIBLE,
+  CONSTRAINT `id_selecao`
+    FOREIGN KEY (`id_selecao`)
+    REFERENCES `mydb`.`selecoes` (`id_selecao`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`estadios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`estadios` (
+  `id_estadio` INT NOT NULL,
+  `nome_estadio` VARCHAR(80) NOT NULL,
+  `cidade` VARCHAR(50) NOT NULL,
+  `pais` VARCHAR(50) NOT NULL,
+  `capacidade` INT NOT NULL,
+  PRIMARY KEY (`id_estadio`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`partidas`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`partidas` (
+  `id_partida` INT NOT NULL,
+  `data_partida` DATE NOT NULL,
+  `id_estadio` INT NOT NULL,
+  `id_selecao_1` INT NOT NULL,
+  `id_selecao_2` INT NOT NULL,
+  `quantidade_gols_selecao_1` INT NOT NULL,
+  `quantidade_gols_selecao_2` INT NOT NULL,
+  `vencedor` INT NULL,
+  PRIMARY KEY (`id_partida`),
+  INDEX `id_estadio_idx` (`id_estadio` ASC) VISIBLE,
+  INDEX `quantidade_gols_selecao_1_idx` (`id_selecao_1` ASC) VISIBLE,
+  INDEX `id_selecao_2_idx` (`id_selecao_2` ASC) VISIBLE,
+  INDEX `vencedor_idx` (`vencedor` ASC) VISIBLE,
+  CONSTRAINT `id_estadio`
+    FOREIGN KEY (`id_estadio`)
+    REFERENCES `mydb`.`estadios` (`id_estadio`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `id_selecao_1`
+    FOREIGN KEY (`id_selecao_1`)
+    REFERENCES `mydb`.`selecoes` (`id_selecao`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `id_selecao_2`
+    FOREIGN KEY (`id_selecao_2`)
+    REFERENCES `mydb`.`selecoes` (`id_selecao`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `vencedor`
+    FOREIGN KEY (`vencedor`)
+    REFERENCES `mydb`.`selecoes` (`id_selecao`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
